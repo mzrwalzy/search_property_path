887a8e096ef31f35a1287d8b1f8c02a8b4165faa
mzrwalzy <69450124+mzrwalzy@users.noreply.github.com>
2021-07-20 18:47:10 +0800

    Add files via upload

--- find_property.py
+++ find_property.py
@@ -2,6 +2,7 @@
 # @Time     : 2021/7/20 17:42
 # @Author   : Charon.
 
+
 def search_property(obj, count, res, arr):
     if count > 8:
         return False
@@ -27,22 +28,3 @@ def search_property(obj, count, res, arr):
                 return True
     else:
         return False
-
-
-if __name__ == '__main__':
-    arr = []
-    obj = {'a': 1, 'b': {'aa': 1, 'bb': [1, 2, {'c': 1}]}, 'cc': {'ccc': {'dd': {'v': [5, 6, {'x': [7]}]}}}}
-    target = 7
-    search_property(obj, 0, target, arr)
-    result = ''
-    for i in arr[::-1]:
-        result += f'[{i}]' if isinstance(i, int) else f'[\'{i}\']'
-    # result += f'[\'{target}\']'
-    print(result)
-    # result = []
-    # for target in arr[::-1]:
-    #     if target == 'aa':
-    #         result.append(target)
-    #         break
-    #     result.append(target)
-    # print(result)
